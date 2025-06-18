f4b1f3956543907f6585e4ae896a40fd8ce681eb
MaguRomera <maguromera@gmail.com>
2025-06-18 14:18:54 -0300

    Funcionalidad botón borrar

--- src/components/DeleteStockButton.jsx
+++ src/components/DeleteStockButton.jsx
@@ -1,10 +1,31 @@
 import deleteicon from "../assets/trash.svg"
-//HACER FUNCIONAL LUEGO DE AGREGAR LA CARGA
-export function DeleteStockItem(){
+import { Navigate } from "react-router-dom";
+import axios from "axios"
+
+export function DeleteStockItem(props){
+
+    const navigate = Navigate
+
+    const handleDelete = async (e) => {
+        e.stopPropagation();
+        const confirmar = window.confirm("¿Seguro que querés eliminar este hilado?");
+        if (confirmar) {
+            try {
+                await axios.delete(`http://localhost:3001/stock/${props.id}`);
+                alert("Hilado eliminado correctamente.");
+                window.location.reload();
+            } catch (error) {
+                console.error("Error al eliminar:", error);
+                alert("Ocurrió un error al eliminar.");
+            }
+        }
+    }
+
     return(
         <button 
             title="Borrar hilado"
-            className="button">
+            className="button"
+            onClick={handleDelete}>
             <img src={deleteicon} className="delete-btn-icon"/>
         </button>
         
--- src/components/StockCard.jsx
+++ src/components/StockCard.jsx
@@ -5,15 +5,19 @@ export function StockCard(props){
     return(
         <div className="stock-card">
             <section className="text-stockcard">
-                <h1>{props.itemstock.material}</h1>
+                <h1>{props.itemstock.material.toUpperCase()}</h1>
                 <span className="info-stockcard">
                     <p>{props.itemstock.color}</p>
                     <p>{props.itemstock.grosor}</p>
                 </span>
             </section>
             <section className="buttons-stockcard">
-                <EditStockItem/>
-                <DeleteStockItem/>
+                <EditStockItem
+                    id = {props.itemstock.id}    
+                />
+                <DeleteStockItem
+                    id = {props.itemstock.id}  
+                />
                 
             </section>
         </div>
