5e02bcdc2305abdf848f88b5ec9567662c7a396a
MaguRomera <maguromera@gmail.com>
2025-09-21 15:33:13 -0300

    Implementación barra de búsqueda de proyectos por nombre

--- src/components/ProyectoSearchingBar.jsx
+++ src/components/ProyectoSearchingBar.jsx
@@ -1,3 +1,24 @@
-export function SearchProyecto(){
-    
+import lupa from '../assets/search.svg'
+import { useState } from 'react'
+export function SearchProyecto({ onSearch }){
+
+    const [search, setSearch] = useState("")
+
+    const handleChange = (e) => {
+        const value = e.target.value
+        setSearch(value)
+        onSearch(value)
+    }
+
+    return(
+        <span className='searching-bar'>
+            <input 
+                type="text" 
+                placeholder="Buscá un proyecto..."
+                value={search}
+                onChange={handleChange}    
+            />
+            <img src={lupa} title='Buscar' />
+        </span>
+    )
 }
